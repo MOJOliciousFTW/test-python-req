86181cd37a42be25860b5160976ca34e90fca331
Johannes Møgster <johannes.mogster@mojotech.no>
2023-06-01 19:25:02 +0200

    Update buildscript.sh

--- scripts/buildscript.sh
+++ scripts/buildscript.sh
@@ -4,6 +4,6 @@ cd Zivid
 wget \
 https://downloads.zivid.com/sdk/releases/2.9.0+4dbba385-1/u22/zivid-telicam-driver_3.0.1.1-3_amd64.deb \
 https://downloads.zivid.com/sdk/releases/2.9.0+4dbba385-1/u22/zivid_2.9.0+4dbba385-1_amd64.deb
-sudo apt install ./*.deb
+apt install ./*.deb
 cd ..
 rm -rf tmp_zivid_deb_install
